214f7c2a3cd80933a566043b0bab310a7749b1bb
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-11-10 18:14:25 -0500

    Tutorial complete

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem "rubocop", "~> 1.68"
+
+gem "google-apis-civicinfo_v2", "~> 0.24.0"
+
+gem "csv", "~> 3.3"
--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,4 @@
-require 'csv'
-puts 'Event Manager Initialized!'
-
-File.exist? 'event_attendees.csv'
+# File.exist? 'event_attendees.csv'
 # contents = File.read('event_attendees.csv')
 # puts contents
 
@@ -10,8 +7,37 @@ File.exist? 'event_attendees.csv'
 #  puts lines
 # end
 
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
 
 puts 'EventManager initialized.'
@@ -22,10 +48,16 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
